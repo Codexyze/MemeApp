75171aa48d419396ceee90bb38129a4fc32d1f28
Akshay Sarapure <akshaysarapure@gmail.com>
2025-01-30 20:03:27 +0530

    Version 1.3                                            #when you click on screen the meme will be refreshed.Long scroll to to enjoy next meme Added share feature click on particular meme to send it to your friend

--- app/src/main/java/com/example/memeapp/ScreenMain.kt
+++ app/src/main/java/com/example/memeapp/ScreenMain.kt
@@ -1,3 +1,4 @@
+import android.content.Intent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
@@ -9,7 +10,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
+import androidx.core.content.ContextCompat.startActivity
 import coil.compose.rememberImagePainter
 
 
@@ -18,6 +21,7 @@ fun Screen(memeViewModel: MemeViewModel) {
     // Get the meme list and error message
     val memeList by memeViewModel.memeList
     val errorMessage by memeViewModel.errorMessage
+    val context = LocalContext.current
 
     // Display memes if the list is not empty
     if (memeList != null && memeList!!.memes.isNotEmpty()) {
@@ -32,7 +36,15 @@ fun Screen(memeViewModel: MemeViewModel) {
                         modifier = Modifier
                             .fillMaxWidth() // Makes the image fill the width of the screen
                             .height(300.dp) // Adjust height as needed, or use dynamic values
-                            .padding(8.dp), // Optional: Padding around the image
+                            .padding(8.dp).clickable {
+                                val shareIntent: Intent = Intent().apply {
+                                    action = Intent.ACTION_SEND
+                                    // Example: content://com.google.android.apps.photos.contentprovider/...
+                                    putExtra(Intent.EXTRA_STREAM, memeItem.url)
+                                    type = "plain/text"
+                                }
+                                context.startActivity(Intent.createChooser(shareIntent, null))
+                            }, // Optional: Padding around the image
                         contentScale = ContentScale.Fit
                     )
                     Spacer(modifier = Modifier.height(8.dp))
